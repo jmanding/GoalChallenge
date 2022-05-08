039f6f31933c24a248efdfa5ef7714a147cb36d2
Nacho <nacho@rethinkmarketing.es>
2022-05-08 13:51:32 +0200

    Añadido handlers de eventos y reestructuración de carpetas

---
+++ GoalChallenge.UnitTest/Handlers/Events/ItemAddInInventoryDomainEventHandle.cs
@@ -0,0 +1,42 @@
+﻿using GoalChallenge.Domain.Events;
+using GoalChallenge.Domain.Models;
+using GoalChallenge.UnitTest.Inventories.Builder;
+using GoalChallenge.UnitTest.Items.Builder;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GoalChallenge.UnitTest.Handlers
+{
+    public class ItemAddInInventoryDomainEventHandle
+    {
+        private ItemAddInInventoryDomainEventHandler _handler;
+        private Mock<Serilog.ILogger> _loggerMock;
+        
+
+        public ItemAddInInventoryDomainEventHandle()
+        {
+            _loggerMock = new Mock<Serilog.ILogger>();
+            _handler = new ItemAddInInventoryDomainEventHandler(_loggerMock.Object);
+        }
+
+        [Fact]
+        public async Task ThrowsExceptionGivenNullEventArgument()
+        {
+            Exception ex = await Assert.ThrowsAsync<ArgumentNullException>(() => _handler.Handle(null, CancellationToken.None));
+        }
+
+        [Fact]
+        public async Task AddItemsToInventoryNotificationHandle()
+        {
+            await _handler.Handle(new ItemAddInInventoryDomainEvent(new Item()), CancellationToken.None);
+
+            _loggerMock.Verify(logger => logger.Information(It.IsAny<string>()));
+        }
+    }
+}
--- GoalChallenge.UnitTest/Items/ItemConstructor.cs
+++ GoalChallenge.UnitTest/Items/ItemConstructor.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Xunit;
 
-namespace GoalChallenge.UnitTest.Inventories
+namespace GoalChallenge.UnitTest.Items
 {
     public class ItemConstructor
     {
